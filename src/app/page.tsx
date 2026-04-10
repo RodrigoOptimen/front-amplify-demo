7e7760ec4956bbdc350fab4a09ae6780b569b68c
Rodrigo <rodrigo.martinez@optimen.com.mx>
2026-04-10 12:36:12 -0600

    Add redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
+import { redirect } from "next/navigation";
+
 
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+  redirect('/dashboard');
 }
